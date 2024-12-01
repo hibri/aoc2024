6e993d4306db88ec372a6722c2b29eb8bd2eccda
Hibri Marzook <hibri@hibri.net>
2024-12-01 19:32:02 +0000

    get the published example working

---
+++ day1.go
@@ -0,0 +1,41 @@
+package aoc2024
+
+import (
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func SortLocations(input []string) ([]int, []int, []int) {
+	var left []int
+	var right []int
+	var diffs []int
+	for index, value := range input {
+		var splitString = strings.Split(value, " ")
+		left = append(left, 0)
+		right = append(right, 0)
+		left[index], _ = strconv.Atoi(splitString[0])
+		right[index], _ = strconv.Atoi(splitString[1])
+	}
+	slices.Sort(left)
+	slices.Sort(right)
+	for index, _ := range left {
+		diff := left[index] - right[index]
+		if diff < 0 {
+			diff *= -1
+		}
+		diffs = append(diffs, diff)
+	}
+
+	return left, right, diffs
+}
+
+func CalculateTotalDistance(input []string) int {
+	total := 0
+	_, _, distances := SortLocations(input)
+	for _, value := range distances {
+		total += value
+	}
+
+	return total
+}
--- day1_test.go
+++ day1_test.go
@@ -3,21 +3,13 @@ package aoc2024
 import (
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
-	"github.com/stretchr/testify/assert"
-	"strconv"
-	"strings"
 	"testing"
 )
-
-func TestAlwaysTrue(t *testing.T) {
-
-	assert.True(t, true)
-}
 
 func TestCanGetLeftAndRightLists(t *testing.T) {
 
 	input := []string{"1 3"}
-	left, right := sortLocations(input)
+	left, right, _ := SortLocations(input)
 	firstLeft := left[0]
 	firstRight := right[0]
 
@@ -28,7 +20,7 @@ func TestCanGetLeftAndRightLists(t *testing.T) {
 func TestCanHandleTwoLinesAsInput(t *testing.T) {
 
 	input := []string{"1 3", "2 4"}
-	left, right := sortLocations(input)
+	left, right, _ := SortLocations(input)
 	firstLeft := left[0]
 	firstRight := right[0]
 
@@ -37,24 +29,34 @@ func TestCanHandleTwoLinesAsInput(t *testing.T) {
 
 }
 
-// func TestDistanceListAreSortedAscending(t *testing.T) {
-//
-//		left, right := sortLocations()
-//		firstLeft := left[0]
-//		firstRight := right[0]
-//
-//		then.AssertThat(t, firstLeft, is.EqualTo(1))
-//		then.AssertThat(t, firstRight, is.EqualTo(3))
-//	}
-func sortLocations(input []string) ([]int, []int) {
-	var left []int
-	var right []int
-	for index, value := range input {
-		var splitString = strings.Split(value, " ")
-		left = append(left, 0)
-		right = append(right, 0)
-		left[index], _ = strconv.Atoi(splitString[0])
-		right[index], _ = strconv.Atoi(splitString[1])
-	}
-	return left, right
+func TestDistanceListAreSortedAscending(t *testing.T) {
+	input := []string{"2 4", "1 3"}
+	left, right, _ := SortLocations(input)
+	firstLeft := left[0]
+	firstRight := right[0]
+
+	then.AssertThat(t, firstLeft, is.EqualTo(1))
+	then.AssertThat(t, firstRight, is.EqualTo(3))
+}
+func TestReturnsDistancesForASimpleList(t *testing.T) {
+	input := []string{"2 4", "1 3"}
+	_, _, distance := SortLocations(input)
+
+	then.AssertThat(t, distance[0], is.EqualTo(2))
+	then.AssertThat(t, distance[1], is.EqualTo(2))
+}
+func TestReturnsDistancesForALongerList(t *testing.T) {
+	input := []string{"3 4", "4 3", "2 5", "1 3", "3 9", "3 3"}
+	_, _, distance := SortLocations(input)
+
+	then.AssertThat(t, distance, is.EqualTo([]int{2, 1, 0, 1, 2, 5}))
+
+}
+
+func TestReturnsTotalDistanceForALongerList(t *testing.T) {
+
+	exptectedTotalDistance := 11
+	input := []string{"3 4", "4 3", "2 5", "1 3", "3 9", "3 3"}
+	totalDistance := CalculateTotalDistance(input)
+	then.AssertThat(t, totalDistance, is.EqualTo(exptectedTotalDistance))
 }
